52f3c20e872337a91b7d839249d3ef752998222a
muzammal kazmi <muzammalkazmi@gmail.com>
2022-02-18 10:59:59 +0500

    component linking

---
+++ src/pages/Sales.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Link} from 'gatsby'
+export default function Sales() {
+  return (
+    <div>
+    <h1>Sales</h1>
+    <Link to='/'>Home</Link>
+    </div>
+  )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import{Link}from 'gatsby'
+
+// markup
+const IndexPage = () => {
+    return ( 
+        <div>
+       <h1> Al Siraj Petroleum </h1>
+       <Link to='/Sales'>Sales</Link>
+       </div>
+    )
+}
+
+export default IndexPage
